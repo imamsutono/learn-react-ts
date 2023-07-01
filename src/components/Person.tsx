7e84d8ae8dae77f1d2257f823db1127ff881dc6e
Imam Sutono <imamsutono>
2023-07-01 20:11:54 +0700

    add usestate with input

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { ChangeEvent, FC, useState } from 'react';
 
 interface Props {
   name: string;
@@ -7,11 +7,22 @@ interface Props {
 }
 
 export const Person: FC<Props> = ({ name, age, email }) => {
+
+  const [country, setCountry] = useState<string | null>("")
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setCountry(event.target.value)
+  }
+
   return (
     <div>
       <h1>{name}</h1>
       <h1>{age}</h1>
       <h1>{email}</h1>
+
+      <input type="text" placeholder="Write down your country..." onChange={handleChange} />
+      <br/>
+      {country}
     </div>
   );
 }
